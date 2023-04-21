023e59e2a5a10d96f035e01176ad1539d2df7380
Maestro-Rus <kirillvolod@gmail.com>
2023-04-21 20:37:13 +0300

    обновил файлы

--- SimpleMathLibrary/SimplyMathLibraryTest/UnitTest1.cs
+++ SimpleMathLibrary/SimplyMathLibraryTest/UnitTest1.cs
@@ -9,39 +9,39 @@ namespace SimplyMathLibraryTest
         [Test]
         public void CalculateCircle()
         {
-            //Наш экземпляр фигуры
+            //Our instance of the figure
             IShape shape = new Circle(10);
 
-            //Вызываем метод для получения площади
+            //Calling the method to get the area
             double area = IShapeResponse.Calculate(shape);
 
-            //Проверка
+            //Check
             Assert.AreEqual(314.1592653589793, area, 0.0001);
 
         }
         [Test]
         public void CalculateTriangle()
         {
-            //Наш экземпляр фигуры
+            //Our instance of the figure
             IShape shape = new Triangle(3,4,5);
 
-            //Вызываем метод для получения площади
+            //Calling the method to get the area
             double area = IShapeResponse.Calculate(shape);
 
-            //Проверка
+            //Check
             Assert.AreEqual(6, area);
 
         }
         [Test]
         public void IsRightTriangle()
         {
-            //Наш экземпляр фигуры
+            //Our instance of the figure
             IShape shape = new Triangle(3, 3, 3);
 
-            //Вызываем метод для получения площади
+            //Calling the method to get the area
             bool area = IShapeResponse.IsRight(shape);
 
-            //Проверка
+            //Check
             Assert.AreEqual(false, area);
 
         }
